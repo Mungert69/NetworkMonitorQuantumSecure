8a9be0dcd1d2a5fc7c27df3ec9d35d9c0eb737f4
Mungert69 <contact@mahadeva.co.uk>
2024-07-07 18:53:02 +0100

    added check host page

--- NetworkMonitorPage.xaml.cs
+++ NetworkMonitorPage.xaml.cs
@@ -1,9 +1,11 @@
-
+using NetworkMonitor.Api.Services;
+using QuantumSecure.ViewModels;
+namespace QuantumSecure;
 public partial class NetworkMonitorPage : ContentPage
 {
-    public NetworkMonitorPage(IApiService apiService)
+    public NetworkMonitorPage(NetworkMonitorViewModel networkMonitorViewModel)
     {
         InitializeComponent();
-        BindingContext = new NetworkMonitorViewModel(apiService);
+        BindingContext = networkMonitorViewModel;
     }
 }
