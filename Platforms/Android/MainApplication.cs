ee85a63c71e522195e0c337d36418f455bf0e1f3
Mungert69 <contact@mahadeva.co.uk>
2025-10-25 16:33:19 +0000

    trying a fix for android 5 and 6 linker not loading the libraries

--- Platforms/Android/MainApplication.cs
+++ Platforms/Android/MainApplication.cs
@@ -1,7 +1,10 @@
-﻿using Android.App;
+﻿using System;
+using System.Linq;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
+using Android.Util;
 using Microsoft.Maui;
 
 namespace QuantumSecure
@@ -18,6 +21,33 @@ namespace QuantumSecure
         {
             base.OnCreate();
 
+            if (Build.VERSION.SdkInt <= BuildVersionCodes.M)
+            {
+                try
+                {
+                    var nativeDir = ApplicationInfo?.NativeLibraryDir;
+                    if (!string.IsNullOrEmpty(nativeDir))
+                    {
+                        var current = Environment.GetEnvironmentVariable("LD_LIBRARY_PATH") ?? string.Empty;
+                        var segments = current
+                            .Split(':', StringSplitOptions.RemoveEmptyEntries)
+                            .ToHashSet(StringComparer.Ordinal);
+
+                        if (segments.Add(nativeDir))
+                        {
+                            var merged = segments.Count == 1
+                                ? nativeDir
+                                : string.Join(":", segments.Prepend(nativeDir));
+                            Environment.SetEnvironmentVariable("LD_LIBRARY_PATH", merged);
+                            Log.Debug("QuantumSecure", $"Primed LD_LIBRARY_PATH with {nativeDir}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Warn("QuantumSecure", $"Unable to prime LD_LIBRARY_PATH: {ex}");
+                }
+            }
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
